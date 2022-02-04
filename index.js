fdc32537fec505ea814e9488fc3ff168beeb458e
Giacomo-Spano <giacomo.spano@libero.it>
2022-02-04 17:49:15 +0100

    1 file modified

--- index.js
+++ index.js
@@ -5,9 +5,23 @@ import { initializeApp } from 'firebase/app';
 
 // Add the Firebase products and methods that you want to use
 import {} from 'firebase/auth';
-import {} from 'firebase/firestore';
+//import {} from 'firebase/firestore';
+
+import { getFirestore, doc, deleteDoc } from 'firebase/firestore';
 
 import * as firebaseui from 'firebaseui';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWj7Ueirn8TX-1YoQXt0NhhM_abxk8GoY",
+  authDomain: "villamaria-e5f51.firebaseapp.com",
+  databaseURL: "https://villamaria-e5f51-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "villamaria-e5f51",
+  storageBucket: "villamaria-e5f51.appspot.com",
+  messagingSenderId: "1009404580063",
+  appId: "1:1009404580063:web:5d3774ff0e9bf56cb9196a",
+  measurementId: "G-ZDRVEK56MS"
+};
 
 // Document elements
 const startRsvpButton = document.getElementById('startRsvp');
@@ -23,13 +37,28 @@ const rsvpNo = document.getElementById('rsvp-no');
 let rsvpListener = null;
 let guestbookListener = null;
 
-let db, auth;
+let  auth;
+
+//const app = initializeApp(firebaseConfig);
+//const db = getFirestore(app);
+
 
 async function main() {
   // Add Firebase project configuration object here
-  const firebaseConfig = {};
+  //const firebaseConfig = {};
 
-  // initializeApp(firebaseConfig);
+  console.log("step 1");
+
+  const app = initializeApp(firebaseConfig);
+  console.log("step 2");
+
+  const db = app.getFirestore();
+  
+
+
+  
+  //const db = getFirestore(app);
+  console.log("step 3");
 
   // FirebaseUI config
   const uiConfig = {
